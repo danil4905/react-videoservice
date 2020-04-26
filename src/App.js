a3521f7b883a03cd72236f594e84be667123b560
Danil <conovalov.danil3441@gmail.com>
2020-04-26 16:31:10 +0400

    Final
    Added Route rtansition

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React from "react";
 import Header from "./components/header/Header";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route} from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Films from "./pages/films/Films";
 import TVchannel from "./pages/TVchannel/TVchannel";
 import Footer from "./components/footer/Footer";
+import { AnimatedSwitch } from "react-router-transition";
 
 function App(props) {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="App">
         <Header />
         <Navbar />
-        <Switch>
+        <AnimatedSwitch
+          atEnter={{ opacity: 0 }}
+          atLeave={{ opacity: 0 }}
+          atActive={{ opacity: 1 }}
+          className="switch-wrapper"
+        >
           <Route path="/" exact>
             <Films state={props.state} />
           </Route>
           <Route path="/channel" exact>
             <TVchannel state={props.state} />
           </Route>
-        </Switch>
+        </AnimatedSwitch>
       </div>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 export default App;
