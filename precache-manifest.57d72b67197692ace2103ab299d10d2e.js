502da51ec5405567e937ff6996b616671e68d31e
Danil <conovalov.danil3441@gmail.com>
2020-10-08 20:42:45 +0400

    Updates

---
+++ precache-manifest.57d72b67197692ace2103ab299d10d2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fdc8ca287733d33cf2aff3b647b405d",
+    "url": "/react-videoservice/index.html"
+  },
+  {
+    "revision": "9ad6799dfc84c603b4a7",
+    "url": "/react-videoservice/static/css/main.ae0308a3.chunk.css"
+  },
+  {
+    "revision": "cf8ea6fe873fe04e017c",
+    "url": "/react-videoservice/static/js/2.8eb05312.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/react-videoservice/static/js/2.8eb05312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ad6799dfc84c603b4a7",
+    "url": "/react-videoservice/static/js/main.001f7c01.chunk.js"
+  },
+  {
+    "revision": "6f1e3bafe19f6631f17b",
+    "url": "/react-videoservice/static/js/runtime-main.65481a89.js"
+  }
+]);
